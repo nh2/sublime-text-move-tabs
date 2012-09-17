cb2b4239863da7fe49a97d02397b39190bb72f15
Niklas Hambüchen <mail@nh2.me>
2012-09-17 17:41:24 +0100

    Add project

---
+++ MoveView.py
@@ -0,0 +1,35 @@
+import sublime
+import sublime_plugin
+
+
+class MoveView(sublime_plugin.WindowCommand):
+
+    def run(self, direction):
+        window = sublime.active_window()
+        view = window.active_view()
+        view_index = window.get_view_index(view)
+
+        if view_index == -1:
+            return
+
+        (group, index) = view_index
+
+        if group < 0 or index < 0:
+            return
+
+        views = window.views_in_group(window.active_group())
+        num_views = len(views)
+        target_index = index
+
+        if direction == 'left':
+            if index > 0:
+                index -= 1
+        elif direction == 'right':
+            if index < num_views - 1:
+                index += 1
+        else:
+            print 'Unrecognized direction:', direction + '. Use left or right.'
+
+        # Move the view
+        if target_index != index:
+            window.set_view_index(view, group, index)
